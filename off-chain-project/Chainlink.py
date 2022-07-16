fff25db0c31d68cd5050957fc273c6155d34e0a8
mrhouzlane <mrhouzlane@github.com>
2022-07-16 21:27:49 +0200

    clean

--- off-chain-project/Chainlink.py
+++ off-chain-project/Chainlink.py
@@ -3,15 +3,22 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 
+# pip install chainlink-feeds
+# pip install websockets
 
-# Chainlink subgraph.
+
+# Chainlink subgraph
 cf = ChainlinkFeeds(output_format='pandas')
 
 # Data returned by the subgraph
-data = cf.get_hourly_candle()
+data = cf.get_hourly_candle(pair='eth/usd')
 data.index = pd.to_datetime(data.index, unit='s')
 
 
+# Converting data to float
 data['closePrice'] = data['closePrice'].astype(float)
 data['averagePrice'] = data['averagePrice'].astype(float)
 data['openPrice'] = data['openPrice'].astype(float)
+
+
+# Plotting
