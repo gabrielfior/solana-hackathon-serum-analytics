b7ce1ca0c61dcb70224ad6bca30c79c7bb5d2d5b
mrhouzlane <mrhouzlane@github.com>
2022-07-16 19:19:05 +0200

    3Prices

--- off-chain-project/Chainlink.py
+++ off-chain-project/Chainlink.py
@@ -5,6 +5,13 @@ import pandas as pd
 cf = ChainlinkFeeds(output_format='pandas')
 data = cf.get_daily_candle(pair='eth/usd')
 data['closePrice'] = data['closePrice'].astype(float)
+data['averagePrice'] = data['averagePrice'].astype(float)
+data['openPrice'] = data['openPrice'].astype(float)
+
+
 data.index = pd.to_datetime(data.index, unit='s')
 
-print(data['closePrice'])
+
+print(data["averagePrice"].plot())
+print(data["closePrice"].plot())
+print(data["openPrice"].plot())
